84e38519419608f121d17b7260efb9f57818d7a9
DESKTOP-KPGNQMS\Dao <1743389563@qq.com>
2023-01-12 21:49:16 +0800

    change Planet.java

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,5 +1,5 @@
 public class Planet {
-    public static double G = 6.67e-11;
+    private static double G = 6.67e-11;
     public double xxPos;//Its current x position
     public double yyPos;
     public double xxVel;//Its current velocity in the x direction
